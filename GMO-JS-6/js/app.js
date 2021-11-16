c4175a2d1dccf61862a0cd19712fc610e4f38f5b
pcminh2710 <phamcminh100@gmail.com>
2021-11-16 16:29:45 +0700

    BaiTap6

---
+++ GMO-JS-6/js/app.js
@@ -0,0 +1,106 @@
+const infoList = []
+//Xử lý add 
+function add() {
+    const date = new Date()
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+    var validate = document.querySelector('.validate')
+    if (name !== "" && phone !== "" && email !== "") {
+        var item = {
+            id: `${date.getHours()}${date.getMinutes()}${date.getMilliseconds()}`,
+            name: name,
+            phone: phone,
+            email: email,
+        }
+        validate.innerHTML = ""
+        infoList.push(item)
+        render();
+        clear();
+    } else {
+        validate.innerHTML = " Vui Lòng nhập đầy đủ các trường này"
+    }
+}
+function clear() {
+    document.getElementById('name').value = "";
+    document.getElementById('phone').value = "";
+    document.getElementById('email').value = "";
+}
+// Xử lý check all
+const checkAllElement = document.querySelector('.checkAll')
+checkAllElement.onclick = function (e) {
+    const cb = document.querySelectorAll('.checkBoxItem');
+    if (e.target.checked === true) {
+        cb.forEach(item => {
+            item.checked = true
+        })
+    } else {
+        cb.forEach(item => {
+            item.checked = false
+        })
+    }
+}
+// delete action
+function deleteItem(id) {
+    const dltI = document.querySelectorAll('.deleteItem');
+    dltI.forEach(() => {
+        infoList.map((item, index) => {
+            if (Number(item.id) === id) {
+                infoList.splice(index, 1)
+            }
+        })
+        render()
+    })
+}
+// delete 
+function remove() {
+    checkAllElement.checked = false
+    const cb = document.querySelectorAll('.checkBoxItem');
+    cb.forEach((item) => {
+        if (item.checked === true) {
+            const indexOfElement = item.parentElement.parentElement.dataset.index
+            infoList.map((item, index) => {
+                if (Number(item.id) === Number(indexOfElement)) {
+                    infoList.splice(index, 1)
+                }
+            })
+            render()
+        }
+    })
+}
+function render() {
+    const list = infoList.map((item) => {
+        return ` <tr data-index="${item.id}">
+        <th class="checkbox">
+            <input type="checkbox" class="checkBoxItem" name="" value="">
+        </th>
+        <th class="name" > 
+            <input type="text" name="name" readonly ondblclick="dblclick(this)" onblur="handleBlur(this)" value="${item.name}">
+        </th>
+        <th class="phone"> 
+            <input type="number" name="phone" readonly ondblclick="dblclick(this)" onblur="handleBlur(this)" value="${item.phone}">
+        </th>
+        <th class="email"> 
+            <input type="text" name="email" readonly ondblclick="dblclick(this)" onblur="handleBlur(this)" value="${item.email}">
+        </th>
+        <th class="action"> 
+            <button class = "deleteItem" onclick ="deleteItem(${item.id})" > delete</button> 
+        </th> 
+    </tr>`
+    }).join("")
+    document.getElementById('render').innerHTML = list;
+}
+
+function dblclick(e) {
+    e.readOnly = false
+}
+
+function handleBlur(e) {
+    e.readOnly = true;
+    const id = e.parentElement.parentElement.dataset.index
+    infoList.forEach(item => {
+        if (Number(item.id) === Number(id)) {
+            item[e.name] = e.value
+        }
+    })
+}
